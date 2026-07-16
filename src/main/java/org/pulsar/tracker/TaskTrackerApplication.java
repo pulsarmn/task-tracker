e6a9d7ed531403aed056c51b1a4228733cb65adc
pulsar <pulsarmn@yandex.ru>
2026-07-16 15:42:08 +0300

    feat: add TaskTrackerApplication launcher

--- src/main/java/org/pulsar/tracker/TaskTrackerApplication.java
+++ src/main/java/org/pulsar/tracker/TaskTrackerApplication.java
@@ -1,12 +1,13 @@
 package org.pulsar.tracker;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class Application {
+public class TaskTrackerApplication {
 
     static void main(String[] args) {
-
+        SpringApplication.run(TaskTrackerApplication.class, args);
     }
 }
