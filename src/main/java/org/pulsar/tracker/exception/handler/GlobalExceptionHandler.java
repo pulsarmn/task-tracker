684f899e07d33cc8559fad3f2046cb728f747f45
pulsar <pulsarmn@yandex.ru>
2026-07-17 10:12:21 +0300

    feat: add exception hadling

--- src/main/java/org/pulsar/tracker/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/org/pulsar/tracker/exception/handler/GlobalExceptionHandler.java
@@ -4,11 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.pulsar.tracker.dto.response.ErrorResponse;
+import org.pulsar.tracker.exception.TaskNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.time.Clock;
 import java.time.Instant;
@@ -21,6 +23,17 @@ public class GlobalExceptionHandler {
 
     private final Clock clock;
 
+    @ExceptionHandler(TaskNotFoundException.class)
+    ResponseEntity<ErrorResponse> handleTaskNotFound(TaskNotFoundException e, HttpServletRequest request) {
+        log.info("Task was not found", e);
+        return build(HttpStatus.NOT_FOUND, "Task was not found", request);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    ResponseEntity<ErrorResponse> handleNoResourceFound(HttpServletRequest request) {
+        return build(HttpStatus.NOT_FOUND, "No resource found", request);
+    }
+
     @ExceptionHandler({MethodArgumentNotValidException.class, IllegalArgumentException.class, NullPointerException.class})
     ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(HttpServletRequest request) {
         // TODO: detailed error message
@@ -29,7 +42,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     ResponseEntity<ErrorResponse> handleException(Exception e, HttpServletRequest request) {
-        log.error("Unexpected error occured on path: {}", request.getRequestURI(), e);
+        log.error("Unexpected error occurred on path: {}", request.getRequestURI(), e);
         return build(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error", request);
     }
 
