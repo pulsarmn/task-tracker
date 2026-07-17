ce04743371845d21b5e57cb1c870fc670899bdff
pulsar <pulsarmn@yandex.ru>
2026-07-17 10:11:06 +0300

    feat: implement a PUT /api/v1/tasks/{id} endpoint for tasks updating

--- src/main/java/org/pulsar/tracker/controller/TaskRestController.java
+++ src/main/java/org/pulsar/tracker/controller/TaskRestController.java
@@ -2,6 +2,7 @@ package org.pulsar.tracker.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.pulsar.tracker.dto.request.TaskCreationRequest;
+import org.pulsar.tracker.dto.request.TaskEditRequest;
 import org.pulsar.tracker.dto.response.TaskCreatedResponse;
 import org.pulsar.tracker.service.TaskService;
 import org.springframework.http.HttpStatus;
@@ -23,6 +24,13 @@ public class TaskRestController {
     ResponseEntity<TaskCreatedResponse> createTask(@Validated @RequestBody TaskCreationRequest taskCreationRequest) {
         TaskCreatedResponse response = taskService.createTask(taskCreationRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<TaskCreatedResponse> updateTask(@PathVariable UUID id,
+                                                   @Validated @RequestBody TaskEditRequest taskEditRequest) {
+        TaskCreatedResponse response = taskService.editTask(id, taskEditRequest);
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/org/pulsar/tracker/service/TaskService.java
+++ src/main/java/org/pulsar/tracker/service/TaskService.java
@@ -3,8 +3,10 @@ package org.pulsar.tracker.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.pulsar.tracker.dto.request.TaskCreationRequest;
+import org.pulsar.tracker.dto.request.TaskEditRequest;
 import org.pulsar.tracker.dto.response.TaskCreatedResponse;
 import org.pulsar.tracker.entity.Task;
+import org.pulsar.tracker.exception.TaskNotFoundException;
 import org.pulsar.tracker.mapper.TaskMapper;
 import org.pulsar.tracker.repository.TaskRepository;
 import org.springframework.stereotype.Service;
@@ -24,8 +26,6 @@ public class TaskService {
 
     @Transactional
     public TaskCreatedResponse createTask(TaskCreationRequest request) {
-        Objects.requireNonNull(request, "TaskCreationRequest must not be null");
-
         Task task = taskMapper.mapToTask(request);
         task = taskRepository.saveAndFlush(task);
         log.info("New task has been successfully saved");
@@ -34,9 +34,22 @@ public class TaskService {
     }
 
     @Transactional
-    public void deleteTask(UUID taskId) {
-        Objects.requireNonNull(taskId, "Task id must not be null");
+    public TaskCreatedResponse editTask(UUID taskId, TaskEditRequest request) {
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(() -> new TaskNotFoundException("The task the with id '{}' was not found"));
+        applyTaskChanges(task, request);
+        task = taskRepository.saveAndFlush(task);
+        return taskMapper.mapToResponse(task);
+    }
 
+    private void applyTaskChanges(Task task, TaskEditRequest request) {
+        task.setTitle(request.title());
+        task.setDescription(request.description());
+        task.setDueDate(request.dueDate());
+    }
+
+    @Transactional
+    public void deleteTask(UUID taskId) {
         taskRepository.deleteById(taskId);
         log.info("Task with id '{}' has been deleted", taskId);
     }
